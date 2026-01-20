14a615f389f0f6c360303d0d91c4db020b0a30f9
Athallahzaki <zapaweh20@gmail.com>
2026-01-21 00:07:23 +0700

    fix docker

--- installers/wings.sh
+++ installers/wings.sh
@@ -75,7 +75,7 @@ dep_install() {
   [ "$CONFIGURE_FIREWALL" == true ] && install_firewall && firewall_ports
 
   case "$OS" in
-  ubuntu | debian | linuxmint)
+  ubuntu | debian)
     install_packages "ca-certificates gnupg lsb-release"
 
     mkdir -p /etc/apt/keyrings
@@ -83,6 +83,16 @@ dep_install() {
 
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$OS \
+      $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
+    ;;
+  linuxmint)
+    install_packages "ca-certificates gnupg lsb-release"
+
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
     ;;
 
